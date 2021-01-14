53e48c0e1a6beb244c6264ea432e5382cdc8f1d8
Nabilara <nabilaturrahma@yahoo.com>
2021-01-14 18:16:19 +0700

    UAS PBO kelompok 4 Nabila

---
+++ checkPanjang.php
@@ -0,0 +1,24 @@
+<?php
+
+class checkPanjang extends mesin{
+
+    private $panjang;
+    private $total;
+
+    public function checkPanjang()
+    {
+        if ($this->panjang < 130)
+        {
+            return ("kata inspirasi terlalu pendek");
+        }
+    }
+    
+    public function checkPanjang2()
+    {
+        if ($this->panjang = 130)
+        {
+            return (total - 3);
+        }
+    }
+
+}
---
+++ mesin.php
@@ -0,0 +1,19 @@
+<?php
+
+class mesin{
+
+    public function cetakKata()
+    {
+
+    }
+
+    public function cetakTambahanBiaya()
+    {
+
+    }
+
+    public function cetakTotalBiaya()
+    {
+
+    }
+}
